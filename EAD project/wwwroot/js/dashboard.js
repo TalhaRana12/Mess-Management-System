e36200aab219c5023e8679bfad12cb284cd77e9c
unknown <talharana193200.5@gmail.com>
2025-11-30 15:10:41 +0500

    Dashboard fix

---
+++ EAD project/wwwroot/js/dashboard.js
@@ -0,0 +1,10 @@
+﻿
+
+// Sidebar Toggle for Mobile
+const sidebarToggle = document.getElementById('sidebarToggle');
+const sidebar = document.querySelector('.sidebar');
+
+    sidebarToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('show');
+});
+
